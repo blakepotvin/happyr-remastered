73363c079aabc148c638cf716fa5ffd4633e900a
Blake Potvin <58959047+Prodxgy@users.noreply.github.com>
2022-05-15 15:00:37 -0400

    added roboto font

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Landing from "/components/landing";
-
+import About from "/components/about";
 export default function Home() {
   return (
     <main>
@@ -10,11 +10,13 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300&family=Roboto&display=swap"
           rel="stylesheet"
-        />
+        ></link>
       </Head>
       <Landing />
+      <About />
+      {/* <a href="https://storyset.com/people">People illustrations by Storyset</a> */}
     </main>
   );
 }
